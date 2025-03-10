230ea66d42ab9c9747211b3378be49d0b3c93c71
mariocollado <mcolladorguez@gmail.com>
2025-03-10 04:40:56 +0100

    Add img/video + styles + section film + form contacto

---
+++ mariocollado_web/src/js/scripts.js
@@ -0,0 +1,24 @@
+// Función para pausar o reproducir un video
+function togglePlayPause(video) {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+// Asignar los eventos de clic a los videos
+document.addEventListener("DOMContentLoaded", () => {
+  const video1 = document.getElementById("video-1");
+  const video2 = document.getElementById("video-2");
+
+  // Asignar evento de play/pause al primer video
+  video1.addEventListener("click", () => {
+    togglePlayPause(video1);
+  });
+
+  // Asignar evento de play/pause al segundo video
+  video2.addEventListener("click", () => {
+    togglePlayPause(video2);
+  });
+});
