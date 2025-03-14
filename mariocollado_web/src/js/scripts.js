ef834402e34c56181137d0489cd57812eaf72b64
mariocollado <mcolladorguez@gmail.com>
2025-03-14 20:58:04 +0100

    add json

--- mariocollado_web/src/js/scripts.js
+++ mariocollado_web/src/js/scripts.js
@@ -1,30 +1,7 @@
-// Función para pausar o reproducir un video
-function togglePlayPause(video) {
-  if (video.paused) {
-    video.play();
-  } else {
-    video.pause();
-  }
-}
-
-// Asignar los eventos de clic a los videos
-document.addEventListener("DOMContentLoaded", () => {
-  const video1 = document.getElementById("video-1");
-  const video2 = document.getElementById("video-2");
-
-  // Asignar evento de play/pause al primer video
-  video1.addEventListener("click", () => {
-    togglePlayPause(video1);
-  });
-
-  // Asignar evento de play/pause al segundo video
-  video2.addEventListener("click", () => {
-    togglePlayPause(video2);
-  });
-});
-
 //MANTENER PULSADO EL BOTÓN DE SCROLL PARA VOLVER AL PRINCIPIO
 import { initHoldButton } from "./holdButton.js";
+
+//ZOOM DE IMÁGENES Y VIDEOS
 
 document.addEventListener("DOMContentLoaded", () => {
   initHoldButton();
