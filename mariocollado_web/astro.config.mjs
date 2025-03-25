16c00a9e7c3b0f1f91edd71984805ec258bc94de
mariocollado <mcolladorguez@gmail.com>
2025-03-25 02:14:16 +0100

    Add new components and update configurations

--- mariocollado_web/astro.config.mjs
+++ mariocollado_web/astro.config.mjs
@@ -1,10 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import alpine from "@astrojs/alpinejs";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [react(), alpine()],
 });
--- mariocollado_web/src/js/filmContent_zoom.js
+++ mariocollado_web/src/js/filmContent_zoom.js
@@ -10,30 +10,20 @@ const content = JSON.parse(
 window.openModal = function (index) {
   const item = content[index];
 
-  if (item.tipo === "foto") {
-    Swal.fire({
-      imageUrl: item.ruta, // Ruta de la imagen
-      imageAlt: item.descripcion, // Texto alternativo
-      imageWidth: "80%", // Ancho de la imagen (80% del modal)
-      imageHeight: "auto", // Altura automática para mantener la proporción
-      showConfirmButton: false, // No mostrar botón de confirmación
-      background: "transparent", // Fondo del modal
-      backdrop: "rgba(0, 0, 0, 0.8)", // Fondo oscuro detrás del modal
-      width: "40em", // Ancho del modal se ajusta al contenido
-      padding: "0", // Sin padding
-      didOpen: () => {
-        // Deshabilitar el scroll del cuerpo
-        document.body.classList.add("no-scroll");
-      },
-      willClose: () => {
-        // Habilitar el scroll del cuerpo al cerrar la modal
-        document.body.classList.remove("no-scroll");
-      },
-    });
-  } else if (item.tipo === "video") {
-    Swal.fire({
-      html: `
-        <video class="w-full h-full object-contain zoomable-video" controls autoplay style="max-width: 80vw; max-height: 80vh;">
+  Swal.fire({
+    html: `
+      ${
+        item.tipo === "foto"
+          ? `
+        <img
+          src="${item.ruta}"
+          alt="${item.descripcion}"
+          class="w-full h-full object-contain zoomable-content"
+          style="max-width: 80vw; max-height: 80vh;"
+        />
+      `
+          : `
+        <video class="w-full h-full object-contain zoomable-content" controls autoplay style="max-width: 80vw; max-height: 80vh;">
           <source src="${item.ruta}" type="video/webm" />
           <source src="${item.ruta.replace(
             ".webm",
@@ -41,34 +31,37 @@ window.openModal = function (index) {
           )}" type="video/mp4" />
           Tu navegador no soporta la reproducción de videos.
         </video>
-      `,
-      showConfirmButton: false, // No mostrar botón de confirmación
-      background: "transparent", // Fondo transparente
-      backdrop: "rgba(0, 0, 0, 0.8)", // Fondo oscuro detrás del modal
-      width: "auto", // Ancho del modal se ajusta al contenido
-      padding: "0", // Sin padding
-      customClass: {
-        popup: "swal2-no-border", // Clase personalizada para el modal
-      },
-      didOpen: () => {
-        // Deshabilitar el scroll del cuerpo
-        document.body.classList.add("no-scroll");
+      `
+      }
+    `,
+    showConfirmButton: false,
+    background: "transparent",
+    backdrop: "rgba(0, 0, 0, 0.8)",
+    width: "auto",
+    padding: "0",
+    customClass: {
+      popup: "swal2-no-border",
+    },
+    didOpen: () => {
+      document.body.classList.add("no-scroll");
 
-        // Agregar evento de doble clic para hacer zoom
-        const video = document.querySelector(".zoomable-video");
-        video.style.transition = "transform 0.25s ease"; // Transición suave
-        video.addEventListener("dblclick", () => {
-          if (video.style.transform === "scale(1.5)") {
-            video.style.transform = "scale(1)"; // Restablecer el zoom
-          } else {
-            video.style.transform = "scale(1.5)"; // Aplicar zoom
-          }
-        });
-      },
-      willClose: () => {
-        // Habilitar el scroll del cuerpo al cerrar la modal
-        document.body.classList.remove("overflow-hidden");
-      },
-    });
-  }
+      const contentElement = document.querySelector(".zoomable-content");
+      let isZoomed = false;
+
+      contentElement.addEventListener("dblclick", () => {
+        if (isZoomed) {
+          contentElement.style.transform = "scale(1)";
+          document.body.classList.remove("no-scroll-zoom");
+        } else {
+          contentElement.style.transform = "scale(1.2)";
+          document.body.classList.add("no-scroll-zoom");
+        }
+        isZoomed = !isZoomed;
+      });
+    },
+    willClose: () => {
+      document.body.classList.remove("no-scroll");
+      document.body.classList.remove("no-scroll-zoom");
+    },
+  });
 };
