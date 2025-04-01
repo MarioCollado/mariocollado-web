94932339368c7d3e59bd80ff36e0143d0c37ec98
mariocollado <mcolladorguez@gmail.com>
2025-04-01 03:17:53 +0200

    Resuelto conflicto en pnpm-lock.yaml

--- mariocollado_web/astro.config.mjs
+++ mariocollado_web/astro.config.mjs
@@ -2,13 +2,15 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import alpine from "@astrojs/alpinejs";
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+    optimizeDeps: {
+      include: ["packery"], // Incluir Packery en la optimización de dependencias de Vite
+    },
   },
 
   integrations: [react(), alpine()],
